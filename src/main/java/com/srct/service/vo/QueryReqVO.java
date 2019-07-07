e5f7acaa1c3e51e79d6e2d882149f4795eac62b9
swansrp <56093273@qq.com>
2019-07-07 22:38:52 +0800

    add order summary feature

--- src/main/java/com/srct/service/vo/QueryReqVO.java
+++ src/main/java/com/srct/service/vo/QueryReqVO.java
@@ -22,9 +22,9 @@ public class QueryReqVO {
     private Integer currentPage;
     private Integer pageSize;
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date queryStartAt;
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date queryEndAt;
 }
